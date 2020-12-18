4abbcf84f31efe562166755e98fb5abdb16256cb
Jake Goulding <jake.goulding@gmail.com>
2020-12-17 22:18:11 -0500

    02b

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,4 +1,4 @@
-use snafu::{ResultExt, Snafu};
+use snafu::{OptionExt, ResultExt, Snafu};
 use std::{convert::TryFrom, ops::RangeInclusive};
 
 #[derive(Debug, Snafu)]
@@ -8,19 +8,31 @@ enum Error {
     MinWasInvalid { source: std::num::ParseIntError },
 
     MaxWasInvalid { source: std::num::ParseIntError },
+
+    CharacterMissing,
 }
 type Result<T, E = Error> = std::result::Result<T, E>;
 
 struct Line<'a> {
     r: RangeInclusive<usize>,
-    c: &'a str,
+    c: char,
     password: &'a str,
 }
 
 impl Line<'_> {
-    fn is_valid(&self) -> bool {
+    fn is_sled_valid(&self) -> bool {
         let occurrences = self.password.matches(self.c).count();
         self.r.contains(&occurrences)
+    }
+
+    fn is_toboggan_valid(&self) -> bool {
+        let location_1 = *self.r.start();
+        let location_2 = *self.r.end();
+
+        let a = self.password.chars().nth(location_1) == Some(self.c);
+        let b = self.password.chars().nth(location_2) == Some(self.c);
+
+        a ^ b
     }
 }
 
@@ -35,20 +47,28 @@ impl<'a> TryFrom<&'a str> for Line<'a> {
         };
         let min = min.parse().context(MinWasInvalid)?;
         let max = max.parse().context(MaxWasInvalid)?;
+        let c = c.chars().next().context(CharacterMissing)?;
         let r = min..=max;
         Ok(Self { r, c, password })
     }
 }
 
-fn process(s: &str) -> Result<usize> {
+fn process_sled_rules(s: &str) -> Result<usize> {
     itertools::process_results(s.lines().map(str::trim).map(Line::try_from), |l| {
-        l.filter(Line::is_valid).count()
+        l.filter(Line::is_sled_valid).count()
+    })
+}
+
+fn process_toboggan_rules(s: &str) -> Result<usize> {
+    itertools::process_results(s.lines().map(str::trim).map(Line::try_from), |l| {
+        l.filter(Line::is_toboggan_valid).count()
     })
 }
 
 fn main() -> Result<()> {
     let input = include_str!("../input.txt");
-    println!("{}", process(input)?);
+    println!("{}", process_sled_rules(input)?);
+    println!("{}", process_toboggan_rules(input)?);
     Ok(())
 }
 
@@ -61,8 +81,14 @@ mod test {
         2-9 c: ccccccccc"#;
 
     #[test]
-    fn demo() -> Result<()> {
-        assert_eq!(process(TEST_INPUT)?, 2);
+    fn demo_sled() -> Result<()> {
+        assert_eq!(process_sled_rules(TEST_INPUT)?, 2);
+        Ok(())
+    }
+
+    #[test]
+    fn demo_toboggan() -> Result<()> {
+        assert_eq!(process_toboggan_rules(TEST_INPUT)?, 1);
         Ok(())
     }
 }
