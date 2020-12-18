2bc41d658f1835f802357e3a53a41533619127e2
Jake Goulding <jake.goulding@gmail.com>
2020-12-17 21:46:55 -0500

    01b

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -9,10 +9,8 @@ enum Error {
 }
 type Result<T, E = Error> = std::result::Result<T, E>;
 
-fn do_it(s: &str) -> Result<u128> {
-    let d = s.lines().map(str::trim).map(str::parse::<u128>);
-    let nums: BTreeSet<_> = itertools::process_results(d, |d| d.filter(|&v| v < 2020).collect())
-        .context(UnableToParseInput)?;
+fn find_pair(s: &str) -> Result<u128> {
+    let nums = parse(s)?;
 
     for n in &nums {
         let comp = 2020 - n;
@@ -24,21 +22,53 @@ fn do_it(s: &str) -> Result<u128> {
     NoResult.fail()
 }
 
+fn find_triple(s: &str) -> Result<u128> {
+    let nums = parse(s)?;
+
+    for a in &nums {
+        for b in nums.range(a..) {
+            let c = 2020 - a - b;
+            if nums.contains(&c) {
+                return Ok(a * b * c);
+            }
+        }
+    }
+
+    NoResult.fail()
+}
+
+fn parse(s: &str) -> Result<BTreeSet<u128>> {
+    let d = s.lines().map(str::trim).map(str::parse::<u128>);
+    itertools::process_results(d, |d| d.filter(|&v| v < 2020).collect()).context(UnableToParseInput)
+}
+
 fn main() -> Result<()> {
     let input = include_str!("../input.txt");
-    println!("{}", do_it(input)?);
+    println!("{}", find_pair(input)?);
+    println!("{}", find_triple(input)?);
     Ok(())
 }
 
-#[test]
-fn demo() -> Result<()> {
-    let input = r#"1721
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const TEST_INPUT: &str = r#"1721
         979
         366
         299
         675
         1456"#;
 
-    assert_eq!(do_it(input)?, 514579);
-    Ok(())
+    #[test]
+    fn test_pair() -> Result<()> {
+        assert_eq!(find_pair(TEST_INPUT)?, 514579);
+        Ok(())
+    }
+
+    #[test]
+    fn test_triple() -> Result<()> {
+        assert_eq!(find_triple(TEST_INPUT)?, 241861950);
+        Ok(())
+    }
 }
